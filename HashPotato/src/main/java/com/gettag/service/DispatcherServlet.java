c7bdef8e33b3977d32bf4d568f3d91850264f56c
princesssuna <1009seona@naver.com>
2020-08-15 15:04:22 +0900

    src/main/java 패키지 클래스 좀 수정했는데 오류나서 주석 처리함.

--- HashPotato/src/main/java/com/gettag/service/DispatcherServlet.java
+++ HashPotato/src/main/java/com/gettag/service/DispatcherServlet.java
@@ -1,5 +1,27 @@
 package com.gettag.service;
 
-public class DispatcherServlet {
+// 유저 요청을 모두 받아 처리하는 서블릿 
+// 아스발 존네 어렵다,, 맨땅에 헤딩
+//public class DispatcherServlet extends HttpServlet{
 
-}
+	/**
+	 * 
+	 */
+//	private static final long serialVersionUID = 1L;
+	
+//	private HandlerMapping mappings;
+	
+//	@Override
+//	public void init(ServletConfig config) throws ServletException {
+//		String urlCtrlName=config.getInitParameter("controllers");
+	
+	
+//	try {
+//		mappings=new HandlerMapping(urlCtrlName);
+//	} catch (Exception e) {
+//		throw new ServletException(e);
+//	}
+//}
+
+
+
