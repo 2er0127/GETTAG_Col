71f06f63eb4a38d516d8771c47346b7881fc33a3
princesssuna <1009seona@naver.com>
2020-08-21 00:24:42 +0900

    mainController add

--- HashPotato/src/main/java/com/gettag/controller/MainController.java
+++ HashPotato/src/main/java/com/gettag/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.gettag.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping("/main")
+	@GetMapping("/") // 루트 경로 /에 HTTP GET 요청 수신되면 이 메소드가 해당 요청을 처리.
 	public String main() {
-		return "/WEB-INF/views/main/mainView.jsp";
+		return "/WEB-INF/views/main/mainView.jsp"; // view 이름을 반환해야한다. 아직 미완성 
 	}
 
 }
+// 메인 홈페이지 컨트롤러 
