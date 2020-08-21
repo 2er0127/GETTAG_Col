c8444cbcf00c269f4f0de2e93541b38afe7df896
shortcutsB <shortcutsb@gmail.com>
2020-08-21 11:25:46 +0900

    frontController webServlet(*com)이랑 web.xml에 있는 dispatcher 서블렛이랑 경로 겹쳐서 톰캣 실행 오류. 주석 처리 해둠

--- HashPotato/src/main/java/com/gettag/controller/FrontController.java
+++ HashPotato/src/main/java/com/gettag/controller/FrontController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.gettag.service.HandlerMapping;
 
 
-@WebServlet("*.com")
+/*@WebServlet("*.com")*/
 public class FrontController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
