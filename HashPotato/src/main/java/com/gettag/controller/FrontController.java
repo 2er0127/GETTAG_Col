4e9b7dce90320007d25c092c92ae39274217dad3
princesssuna <1009seona@naver.com>
2020-08-24 04:02:08 +0900

    controller 변경

--- HashPotato/src/main/java/com/gettag/controller/FrontController.java
+++ HashPotato/src/main/java/com/gettag/controller/FrontController.java
@@ -1,8 +1,19 @@
 package com.gettag.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class FrontController {
-	
+	@RequestMapping(value="/", method=RequestMethod.GET) // value값 다시 지정 
+	public String main() {
+		
+		return ""; // return값 지정
+	}
 }
-			
+// main 메소드 안에 들어갈 것
+// 1.
+// 2.
 
 	
