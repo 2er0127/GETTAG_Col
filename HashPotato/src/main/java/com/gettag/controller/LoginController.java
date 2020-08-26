a6743cc6fae0ccf7548b9b0ca5f9b74106871793
princesssuna <1009seona@naver.com>
2020-08-26 17:02:23 +0900

    loginController add

--- HashPotato/src/main/java/com/gettag/controller/LoginController.java
+++ HashPotato/src/main/java/com/gettag/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.gettag.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	public void main() {
-		
+	@RequestMapping("/")
+	public String main() {
+		return "main/mainView";
+	}
+	
+	@RequestMapping("/loginView")
+	public String login() {
+		return "login/loginView"; // WEB-INF/viewss/login/loginView.jsp를 찾는다.
 	}
 
-}
 
-// WHYRANO
+}
