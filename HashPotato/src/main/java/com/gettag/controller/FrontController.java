efe238abd827d28dff3c854c8e64b1c2f805bc52
princesssuna <1009seona@naver.com>
2020-08-27 23:32:09 +0900

    controller, bean 파일 추가

--- HashPotato/src/main/java/com/gettag/controller/FrontController.java
+++ HashPotato/src/main/java/com/gettag/controller/FrontController.java
@@ -8,19 +8,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FrontController {
-	@RequestMapping(value="/", method=RequestMethod.GET) // value값 다시 지정 
-	public ModelAndView main(ModelAndView mv) {
+//	@RequestMapping(value="/", method=RequestMethod.GET) // value값 다시 지정 
+//	public ModelAndView main(ModelAndView mv) {
 		
-		mv.setViewName("main/mainView");
-		return mv; // GET mainView
-	}
-	@RequestMapping(value="/gSearch/{gSearchView}", method=RequestMethod.POST) // gSearch에 action값으로 설정해주기!
-	public ModelAndView gSearch(ModelAndView mv, @PathVariable int gSearchView) {
+//		mv.setViewName("main/mainView");
+//		return mv; // GET mainView
+//	}
+//	@RequestMapping(value="/gSearch/{gSearchView}", method=RequestMethod.POST) // gSearch에 action값으로 설정해주기!
+//	public ModelAndView gSearch(ModelAndView mv, @PathVariable int gSearchView) {
 		
-		// 여기에 검색 버튼을 눌렀을 때? 가 들어가야함 (여기 아니면 위에)
-		mv.setViewName("gSearch/gSearchView");
-		return mv; // POST gSearch   
-	}
+//		// 여기에 검색 버튼을 눌렀을 때? 가 들어가야함 (여기 아니면 위에)
+//		mv.setViewName("gSearch/gSearchView");
+//		return mv; // POST gSearch   
+//	}
 }
 
 // mainView -> gSearch
--- HashPotato/src/main/java/com/gettag/controller/MainController.java
+++ HashPotato/src/main/java/com/gettag/controller/MainController.java
@@ -1,28 +1,20 @@
 package com.gettag.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class MainController {
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MainController implements Controller{
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView main(ModelAndView mv) {
-		
-		mv.setViewName("main/mainView");
-		return mv;
-	}
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView main2(ModelAndView mv) {
-		
-		mv.setViewName("main/mainView");
-		return mv;
-	}
+	public String handleRequest(HttpServletRequest request
+			  , HttpServletResponse response
+			  ) throws Exception {
+return "/WEB-INF/views/main/mainView.jsp";
+}
 
 }
 
 // 메인 홈페이지 컨트롤러 
-// 로고 -> 자기자신 소
+// 로고 -> 자기자신 소환   
 
