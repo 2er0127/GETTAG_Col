a58fcbcdd17032b08169e11924310f22828926f8
ycyeon <ickite428@gmail.com>
2020-08-30 14:56:53 +0900

    GameInfo(게임검색결과) dao,vo 추가

---
+++ HashPotato/src/main/java/com/gettag/vo/GameInfoVO.java
@@ -0,0 +1,35 @@
+package com.gettag.vo;
+
+public class GameInfoVO {
+	private String gTitle;
+	private String gRelease;
+	private String gImage;
+	private String gAppid;
+	
+	public String getgTitle() {
+		return gTitle;
+	}
+	public void setgTitle(String gTitle) {
+		this.gTitle = gTitle;
+	}
+	public String getgRelease() {
+		return gRelease;
+	}
+	public void setgRelease(String gRelease) {
+		this.gRelease = gRelease;
+	}
+	public String getgImage() {
+		return gImage;
+	}
+	public void setgImage(String gImage) {
+		this.gImage = gImage;
+	}
+	public String getgAppid() {
+		return gAppid;
+	}
+	public void setgAppid(String gAppid) {
+		this.gAppid = gAppid;
+	}
+	
+	
+}
