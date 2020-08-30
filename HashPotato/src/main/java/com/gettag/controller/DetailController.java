07d26cb8586163b73a3416f00ff7c2d01779ee8a
princesssuna <1009seona@naver.com>
2020-08-30 16:08:20 +0900

    공지사항 컨트롤러 만드는 중 DetailController

--- HashPotato/src/main/java/com/gettag/controller/DetailController.java
+++ HashPotato/src/main/java/com/gettag/controller/DetailController.java
@@ -1,14 +1,32 @@
 package com.gettag.controller;
 
+//  import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.gettag.dao.NoticeDAO;
+//import com.gettag.vo.NoticeVO;
+
 
 public class DetailController implements Controller{
 	public String handleRequest(HttpServletRequest request
 			  , HttpServletResponse response
 			  ) throws Exception {
 
-		return "/WEB-INF/views/gRecommend/gRecommendView.jsp";
-}
+		
+		int no = Integer.parseInt(request.getParameter("no"));
+		String type = request.getParameter("type");
 
+		NoticeDAO dao = new NoticeDAO();
+		
+		
+				if(type != null && type.equals("list")) {
+					dao.updateViewCnt(no);
+				}
+				
+
+
+		return "/WEB-INF/views/notice/noticeWriteView.jsp";
+	}
 }
